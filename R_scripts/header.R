f353538b9773850cda27ce687b13f4860169f132
langhuai94 <1939747262@qq.com>
2025-03-13 01:11:49 +0800

    create folders

---
+++ R_scripts/header.R
@@ -0,0 +1,16 @@
+# HEADER --------------------------------------------
+#
+# Author:     Sihan Wang
+# Copyright     Copyright 2025 - Name Surname
+# Email:      1939747262@qq.com
+#
+# Date:     2025-03-13
+#
+# Script Name:    
+#
+# Script Description:
+#
+#
+# SETUP ------------------------------------
+cat("\014")                 # Clears the console
+rm(list = ls())             # Remove all variables of the work space
